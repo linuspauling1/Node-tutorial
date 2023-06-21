3a5c8799a248b339bec8f24cb7be8611620df7bb
linuspauling1 <paulb2233@yahoo.com>
2023-06-22 01:00:52 +0300

    Refurbished

--- app3.js
+++
@@ -1,9 +0,0 @@
-const http = require('http')
-const server = http.createServer((request,response) => {
-    console.log('Server exists!')
-    response.end('Welcome!')
-})
-server.listen(5000)
-setInterval(() => {
-    console.log('After server!')
-}, 1000);
